e9d8c7690c547df4b6f87a322af623206b0deb0b
Arturo Jiménez de los Galanes Reguillos <arturo.jimenezdelosgalanes@outsourcing.moeveglobal.com>
2025-12-18 12:14:59 +0100

    Update token URL

--- lib/configuration/constants.dart
+++ lib/configuration/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const SERVER_URL = 'wss://meet.movilok.net'; // Replace with your LiveKit server URL
-  static const TOKEN_ENDPOINT = 'http://192.168.1.171:3000/getToken'; //'http://localhost:3000/getToken'; // Replace with your token server URL
+  static const TOKEN_ENDPOINT = 'https://token-611429764129.europe-southwest1.run.app'; //'http://localhost:3000/getToken'; // Replace with your token server URL
 
 
   static const bool E2EE = false;
